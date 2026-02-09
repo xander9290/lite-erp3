098bac0168ad8b1023e3fa31e1a3a696c81a1a97
xander9290 <53929034+xander9290@users.noreply.github.com>
2026-02-08 22:22:01 -0600

    Revert "instalación de prisma"
    This reverts commit 4876a5d71771c5a97df197e395d9fde0ac5ba88a.

--- app/actions/user-actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { prisma } from "@/libs/prisma";
-import { Partner, User } from "@prisma/client";
-
-export interface UserWithPartner extends User {
-  Partner?: Partner;
-}
--- prisma/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@prisma/config";
-
-export default defineConfig({
-  schema: "./schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL || "", // 👈 antes iba en el schema, ahora aquí
-  },
-});
