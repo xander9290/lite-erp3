ac68e45146e3c609fcb8952938200fba975de4e6
Alex090290 <154947569+Alex090290@users.noreply.github.com>
2026-03-23 11:31:30 -0600

    Reset db

--- prisma/migrations/20260307022621_add_manager_to_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "manager_id" TEXT;
-
--- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_manager_id_fkey" FOREIGN KEY ("manager_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
