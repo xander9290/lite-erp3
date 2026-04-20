a09ec19c911ef8f66d15df3cb14e3abe651c1424
xander9290 <53929034+xander9290@users.noreply.github.com>
2026-04-19 23:11:39 -0600

    Producttemplate
    Segunda fase en proceso

--- app/(auth)/app/product_template/views/ProductTemplateFormView.tsx
+++ app/(auth)/app/product_template/views/ProductTemplateFormView.tsx
@@ -11,6 +11,19 @@ import {
 import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { useModals } from "@/contexts/ModalContext";
+import {
+  FormView,
+  FormViewGroup,
+  FormViewStack,
+} from "@/components/templates/FormView";
+import {
+  FieldBoolean,
+  FieldEntry,
+  FieldImage,
+  FieldRelation,
+  FieldSelect,
+} from "@/components/templates/fields";
+import { Notebook, Page, PageSheet } from "@/components/templates/Notebook";
 
 function ProductTemplateFormView({
   id,
@@ -80,7 +93,110 @@ function ProductTemplateFormView({
     };
   }, [product, reset]);
 
-  return <div>ProductTemplateFormView</div>;
+  return (
+    <FormView
+      methods={methods}
+      cleanUrl="/app/product_template?view_type=form&id=null"
+      auditLog="productTemplate"
+      reverse={handleReverse}
+      onSubmit={onSubmit}
+      id={id}
+    >
+      <FormViewGroup>
+        <FieldImage folder="products" name="imageUrl" remove editable />
+        <FieldEntry name="description" label="Descripción" />
+        <FieldEntry name="defaultCode" label="Referencia interna" />
+        <FieldSelect
+          name="displayType"
+          options={[
+            { label: "Producto", value: "PRODUCT" },
+            { label: "Servicio", value: "SERVICE" },
+            { label: "Consumible", value: "CONSU" },
+          ]}
+          label="Tipo"
+        />
+        <FieldBoolean name="active" label="Activo" />
+      </FormViewGroup>
+      <Notebook defaultActiveKey="purchases">
+        <Page eventKey="purchases" title="Compras">
+          <PageSheet name="purchase">
+            <FormViewGroup>
+              <FieldBoolean name="purchases" label="Se puede comprar" />
+              <FormViewStack>
+                <FieldEntry
+                  name="lastCost"
+                  type="number"
+                  label="Último costo"
+                  readonly
+                  className="text-end"
+                />
+                <FieldEntry
+                  name="uomIncomingAllowed"
+                  type="number"
+                  label="Múltiplo de compra"
+                  className="text-center"
+                />
+              </FormViewStack>
+              <FieldRelation model="user" name="userId" label="Comprador" />
+              <FieldRelation
+                model="partner"
+                name="supplierId"
+                label="Proveedor"
+                domain={[["displayType", "=", "SUPPLIER"]]}
+              />
+            </FormViewGroup>
+          </PageSheet>
+        </Page>
+        <Page eventKey="sales" title="Ventas">
+          <PageSheet name="sales">
+            <FormViewGroup>
+              <FieldBoolean name="sales" label="Se puede vender" />
+              <FormViewStack>
+                <FieldEntry
+                  type="number"
+                  name="price1"
+                  label="Precio 1"
+                  className="text-end"
+                />
+                <FieldEntry
+                  type="number"
+                  name="price2"
+                  label="Precio 2"
+                  className="text-end"
+                />
+                <FieldEntry
+                  type="number"
+                  name="price3"
+                  label="Precio 3"
+                  className="text-end"
+                />
+              </FormViewStack>
+              <FormViewStack>
+                <FieldEntry
+                  type="number"
+                  name="price4"
+                  label="Precio 4"
+                  className="text-end"
+                />
+                <FieldEntry
+                  type="number"
+                  name="price5"
+                  label="Precio 5"
+                  className="text-end"
+                />
+              </FormViewStack>
+              <FieldEntry
+                name="uomOutgoingAllowed"
+                type="number"
+                label="Múltiplo de venta"
+                className="text-center"
+              />
+            </FormViewGroup>
+          </PageSheet>
+        </Page>
+      </Notebook>
+    </FormView>
+  );
 }
 
 export default ProductTemplateFormView;
