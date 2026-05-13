da056e0ce48a11941a9e4bccb140138776d87cc6
Alex090290 <154947569+Alex090290@users.noreply.github.com>
2026-05-13 12:33:02 -0600

    ALGO

--- app/(auth)/app/product_template/products/views/ProductTemplateFormView.tsx
+++ app/(auth)/app/product_template/products/views/ProductTemplateFormView.tsx
@@ -1,18 +1,43 @@
 "use client";
 
-import { createProduct, ProductTemplateWithProps, updateProduct } from "../actions/productTemplate.action";
+import {
+  createProduct,
+  ProductTemplateWithProps,
+  updateProduct,
+} from "../actions/productTemplate.action";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { productTemplateSchema, productTemplateSchemaDefault, ProductTemplateSchemaType } from "../schemas/productTemplate.schema";
+import {
+  productTemplateSchema,
+  productTemplateSchemaDefault,
+  ProductTemplateSchemaType,
+} from "../schemas/productTemplate.schema";
 import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { useModals } from "@/contexts/ModalContext";
-import { FormView, FormViewGroup, FormViewStack } from "@/components/templates/FormView";
-import { FieldBoolean, FieldEntry, FieldImage, FieldRelation, FieldSelect, FieldTags } from "@/components/templates/fields";
+import {
+  FormView,
+  FormViewGroup,
+  FormViewStack,
+} from "@/components/templates/FormView";
+import {
+  FieldBoolean,
+  FieldEntry,
+  FieldImage,
+  FieldRelation,
+  FieldSelect,
+  FieldTags,
+} from "@/components/templates/fields";
 import { Notebook, Page, PageSheet } from "@/components/templates/Notebook";
 import toast from "react-hot-toast";
 
-function ProductTemplateFormView({ id, product }: { id: string | null; product: ProductTemplateWithProps | null }) {
+function ProductTemplateFormView({
+  id,
+  product,
+}: {
+  id: string | null;
+  product: ProductTemplateWithProps | null;
+}) {
   const methods = useForm<ProductTemplateSchemaType>({
     resolver: zodResolver(productTemplateSchema),
     defaultValues: productTemplateSchemaDefault,
@@ -30,7 +55,9 @@ function ProductTemplateFormView({ id, product }: { id: string | null; product: 
       const res = await createProduct({ data });
       if (!res.success) return modalError(res.message);
 
-      router.replace(`/app/product_template/products?view_type=form&id=${res.data?.id}`);
+      router.replace(
+        `/app/product_template/products?view_type=form&id=${res.data?.id}`,
+      );
       toast.success(res.message);
     } else {
       const res = await updateProduct({ data, id });
@@ -186,11 +213,28 @@ function ProductTemplateFormView({ id, product }: { id: string | null; product: 
             <FormViewGroup>
               <FieldBoolean name="purchases" label="Se puede comprar" />
               <FormViewStack>
-                <FieldEntry name="lastCost" type="number" label="Último costo" readonly className="text-end" />
-                <FieldEntry name="uomIncomingAllowed" type="number" label="Múltiplo de compra" className="text-center" step={0.0001} />
+                <FieldEntry
+                  name="lastCost"
+                  type="number"
+                  label="Último costo"
+                  readonly
+                  className="text-end"
+                />
+                <FieldEntry
+                  name="uomIncomingAllowed"
+                  type="number"
+                  label="Múltiplo de compra"
+                  className="text-center"
+                  step={0.0001}
+                />
               </FormViewStack>
               <FieldRelation model="user" name="userId" label="Comprador" />
-              <FieldRelation model="partner" name="supplierId" label="Proveedor" domain={[["displayType", "=", "SUPPLIER"]]} />
+              <FieldRelation
+                model="partner"
+                name="supplierId"
+                label="Proveedor"
+                domain={[["displayType", "=", "SUPPLIER"]]}
+              />
             </FormViewGroup>
           </PageSheet>
         </Page>
@@ -199,14 +243,50 @@ function ProductTemplateFormView({ id, product }: { id: string | null; product: 
             <FormViewGroup>
               <FieldBoolean name="sales" label="Se puede vender" />
               <FormViewStack>
-                <FieldEntry type="number" name="price1" label="Precio 1" className="text-end" step={0.01} />
-                <FieldEntry type="number" name="price2" label="Precio 2" className="text-end" step={0.01} />
-                <FieldEntry type="number" name="price3" label="Precio 3" className="text-end" step={0.01} />
+                <FieldEntry
+                  type="number"
+                  name="price1"
+                  label="Precio 1"
+                  className="text-end"
+                  step={0.01}
+                />
+                <FieldEntry
+                  type="number"
+                  name="price2"
+                  label="Precio 2"
+                  className="text-end"
+                  step={0.01}
+                />
+                <FieldEntry
+                  type="number"
+                  name="price3"
+                  label="Precio 3"
+                  className="text-end"
+                  step={0.01}
+                />
               </FormViewStack>
               <FormViewStack>
-                <FieldEntry type="number" name="price4" label="Precio 4" className="text-end" step={0.01} />
-                <FieldEntry type="number" name="price5" label="Precio 5" className="text-end" step={0.01} />
-                <FieldEntry name="uomOutgoingAllowed" type="number" label="Múltiplo de venta" className="text-center" step={0.0001} />
+                <FieldEntry
+                  type="number"
+                  name="price4"
+                  label="Precio 4"
+                  className="text-end"
+                  step={0.01}
+                />
+                <FieldEntry
+                  type="number"
+                  name="price5"
+                  label="Precio 5"
+                  className="text-end"
+                  step={0.01}
+                />
+                <FieldEntry
+                  name="uomOutgoingAllowed"
+                  type="number"
+                  label="Múltiplo de venta"
+                  className="text-center"
+                  step={0.0001}
+                />
               </FormViewStack>
             </FormViewGroup>
           </PageSheet>
