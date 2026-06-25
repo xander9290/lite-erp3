6a8d5d0b9375f3a805063e25eea8ae8b40bacf40
Alex090290 <154947569+Alex090290@users.noreply.github.com>
2026-06-25 09:14:41 -0600

    some fixes

--- components/templates/fields/FieldText.tsx
+++ components/templates/fields/FieldText.tsx
@@ -2,7 +2,17 @@
 
 import { useAccess } from "@/contexts/AccessContext";
 
-export function FieldText({ name, invisible, output, className }: { name: string; invisible?: boolean; output: string; className?: string }) {
+export function FieldText({
+  name,
+  invisible,
+  output,
+  className,
+}: {
+  name: string;
+  invisible?: boolean;
+  output: string | null | undefined;
+  className?: string;
+}) {
   const access = useAccess({ fieldName: name });
 
   if (invisible) return null;
@@ -10,7 +20,7 @@ export function FieldText({ name, invisible, output, className }: { name: string
 
   return (
     <span className={`text-truncate m-0 ${className}`} title={name}>
-      {output}
+      {output ? output : null}
     </span>
   );
 }
